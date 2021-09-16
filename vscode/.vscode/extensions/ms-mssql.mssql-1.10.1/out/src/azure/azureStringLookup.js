58fe5ba9fe202c13c7edbba5a0d9f4a0f5ac1bb5
Mr Dimples <mrdimples@dankbook.hsd1.wa.comcast.net>
2021-09-16 16:06:07 -0700

    added i3 removed vscode

--- vscode/.vscode/extensions/ms-mssql.mssql-1.10.1/out/src/azure/azureStringLookup.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AzureStringLookup = void 0;
-class AzureStringLookup {
-}
-exports.AzureStringLookup = AzureStringLookup;
-
-//# sourceMappingURL=azureStringLookup.js.map
